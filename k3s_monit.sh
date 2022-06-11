2ca1c666da221e7a08de96678f133034b6246733
dmpe <cincenko@outlook.com>
2022-06-11 21:55:52 +0200

    basic setup is now orking, with pvc and pv

--- k3s_monit.sh
+++ k3s_monit.sh
@@ -6,7 +6,8 @@ echo "Install Monitoring - Grafana"
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
-helm template grafana grafana/grafana -f grafana/values.yaml
-helm upgrade --create-namespace --install abc grafana/grafana -f grafana/values.yaml
+# helm template grafana grafana/grafana -f grafana/values.yaml
+k3s kubectl apply -f grafana/storage.yaml
+helm upgrade --create-namespace --install grafana grafana/grafana -f grafana/values.yaml
 
 xdg-open http://grafana.localhost/
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # Demo
 
 Running k3s kubernetes cluster, with exposed Traefik dashboard, and AWX operator using its ingress.
+
+# Storage
+
+Look in 
+
+```
+cd /var/lib/rancher/k3s/storage
+```
