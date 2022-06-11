82d2408dfeb17ce3b03e52c9276d0dfab89fb8ec
dmpe <cincenko@outlook.com>
2022-06-11 22:36:09 +0200

    add local versions of grafana and rancher-monitoring

--- k3s.sh
+++ k3s.sh
@@ -5,16 +5,19 @@ curl -sfL https://get.k3s.io | sh -
 sudo chown $USER:$USER -R /etc/rancher/k3s/
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
+echo "Setup k8s dashboard on localhost."
 GITHUB_URL=https://github.com/kubernetes/dashboard/releases
 VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
 k3s kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
 
-k3s kubectl create -f dashboard.yaml
+k3s kubectl create -f charts/k3s/dashboard.yaml
 k3s kubectl -n kubernetes-dashboard describe secret admin-user-token | grep '^token'
 k3s kubectl proxy &
 xdg-open http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
 
 echo "Setup traefik dashboard on localhost."
-kubectl apply -f traefik.yaml
+kubectl apply -f charts/traefik/traefik.yaml
 xdg-open http://traefik.localhost/dashboard/
 
+echo "Apply storage class for local host"
+kubectl apply -f sc.yaml
--- k3s_monit.sh
+++ k3s_monit.sh
@@ -4,10 +4,16 @@ source ./k3s.sh
 
 echo "Install Monitoring - Grafana"
 helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add rancher http://charts.rancher.io/
 helm repo update
 
-# helm template grafana grafana/grafana -f grafana/values.yaml
-k3s kubectl apply -f grafana/storage.yaml
-helm upgrade --create-namespace --install grafana grafana/grafana -f grafana/values.yaml
+# helm pull rancher/rancher-monitoring-crd --untar --untardir .
+# helm pull rancher/rancher-monitoring --untar --untardir .
+# helm pull grafana/grafana --untar --untardir .
+
+# helm template grafana ./charts/grafana -f ./charts/grafana/values.yaml -f ./charts/grafana/MY_values.yaml
+helm upgrade --create-namespace --install grafana ./charts/grafana -f ./charts/grafana/values.yaml -f ./charts/grafana/MY_values.yaml
+helm upgrade --create-namespace --install rancher-monitoring-crd ./charts/rancher-monitoring-crd -f ./charts/rancher-monitoring-crd/values.yaml
+helm upgrade --create-namespace --install rancher-monitoring ./charts/rancher-monitoring -f ./charts/rancher-monitoring/values.yaml
 
 xdg-open http://grafana.localhost/
