e613883358e3a63f8ce88c37ce7d254d32c9ef88
Ubuntu <azureuser@ubuntu-testing.3pqkxl4odrme5jsfcdobyernva.ex.internal.cloudapp.net>
2024-04-20 20:37:16 +0000

    add new kubernetes dahboard

--- k3s-start.sh
+++ k3s-start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# on azure VMs, must run with
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-backend=none --disable-network-policy" sh -
+
+
+# normally ...
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-backend=none --disable-network-policy --disable=servicelb --disable=traefik" sh -
+
 sudo kubectl --kubeconfig /etc/rancher/k3s/k3s.yaml create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/tigera-operator.yaml
 sudo kubectl --kubeconfig /etc/rancher/k3s/k3s.yaml create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/custom-resources.yaml
 
