3dd792af848def24ce6161b11bf559ae94593ec7
dmpe <cincenko@outlook.com>
2024-04-20 21:17:07 +0000

    stage

--- k3s-traefik.sh
+++ k3s-traefik.sh
@@ -4,6 +4,9 @@ echo "Setup traefik dashboard on localhost."
 
 helm upgrade --install --create-namespace -n traefik traefik ./charts/traefik-external/traefik
 
+# for azure where it needs to run with integrated traefik on k3s
+kubectl apply -f charts/traefik-internal/traefik-config.yaml
+
 kubectl apply -f charts/traefik-internal/dashboard.yaml
 xdg-open http://traefik.localhost:9000/dashboard
 
